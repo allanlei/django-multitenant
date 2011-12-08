f326dd569e9240c2b883e9c5f436728f321a0c61
Allan Lei <allanlei@helveticode.com>
2011-12-08 15:36:27 -0800

    Add TransactionMiddleware

--- tenant/middleware.py
+++ tenant/middleware.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import resolve
 from django.shortcuts import get_object_or_404
+from django.db import transaction
 
 from tenant.models import Tenant
 from tenant.utils import connect_tenant_provider, disconnect_tenant_provider
@@ -18,4 +19,31 @@ class TenantMiddleware(object):
     def process_response(self, request, response):
         disconnect_tenant_provider(request)
         request.tenant = None
+        return response
+
+
+
+class TransactionMiddleware(object):
+    def get_tenant(self, request):
+        tenant = getattr(request, 'tenant', None)
+        if tenant:
+            return tenant.ident
+        
+    def process_request(self, request):
+        """Enters transaction management"""
+        transaction.enter_transaction_management(using=self.get_tenant(request))
+        transaction.managed(True, using=self.get_tenant(request))
+
+    def process_exception(self, request, exception):
+        """Rolls back the database and leaves transaction management"""
+        if transaction.is_dirty(using=self.get_tenant(request)):
+            transaction.rollback(using=self.get_tenant(request))
+        transaction.leave_transaction_management(using=self.get_tenant(request))
+
+    def process_response(self, request, response):
+        """Commits and leaves transaction management."""
+        if transaction.is_managed(using=self.get_tenant(request)):
+            if transaction.is_dirty(using=self.get_tenant(request)):
+                transaction.commit(using=self.get_tenant(request))
+            transaction.leave_transaction_management(using=self.get_tenant(request))
         return response
