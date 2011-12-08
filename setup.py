01865be1953ba091227d16003423660ae87d7fad
Allan Lei <allanlei@helveticode.com>
2011-12-08 15:36:53 -0800

    Update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def find_packages_in(where, **kwargs):
 
 setup(
     name = 'django-multitenant',
-    version = '0.2.0',
+    version = '0.2.1',
     author = 'Allan Lei',
     author_email = 'allanlei@helveticode.com',
     description = ('Multi-tenant addons for Django'),
